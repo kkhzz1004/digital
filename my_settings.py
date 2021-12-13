9587569e8ceedcc8208c56c4c8efeee4970e46ed
Will(keonhwan kim) <30832990+kkhzz1004@users.noreply.github.com>
2021-12-14 05:01:16 +0900

    'db'

--- my_settings.py
+++ my_settings.py
@@ -1,10 +1,20 @@
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'test_db',
+#         'HOST': '127.0.0.1',
+#         'PORT': 27017,
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'test_db',
-        'HOST': '127.0.0.1',
-        'PORT': 27017,
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '82ce304c97d30efb8c2d8dd5c86d83d9',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
-
